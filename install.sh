d8f55f0463e09413c37c50e89e5e6049cce30eb3
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-07-28 22:11:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo apt update && \
-sudo apt install xfce4 xfce4-goodies -y && \
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
-sudo dpkg -i google-chrome-stable_current_amd64.deb && \
-sudo apt --fix-broken install -y && \
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && \
-sudo dpkg -i chrome-remote-desktop_current_amd64.deb && \
-sudo apt --fix-broken install -y && \
+sudo apt update && sudo apt upgrade -y
+sudo apt install xfce4 xfce4-goodies -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install -y
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome-remote-desktop_current_amd64.deb
+sudo apt --fix-broken install -y
 echo "exec /usr/bin/xfce4-session" > ~/.chrome-remote-desktop-session
